a3c73bd5cbc30109a7d483f7f4b0e24c19a7e0c0
Alissa Lawrence <alissa.lawrence@macys.com>
2015-10-26 09:47:56 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+function handleResponse(req, res){
+   var path = req.url;
+   var response = "hello world";
+   
+   switch(path){
+      case "/testingCallback":
+         console.log("req is: " + req);
+         response = "Thanks for calling me back!";
+         break;
+      default:
+         break;
+   }
+
+   res.writeHead(200);
+   res.end(response);
+}
+
+var server = http.createServer(handleResponse);
+
+server.listen(8080, function(){
+   console.log("listening on PORT 8080");
+});   
