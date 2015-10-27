fe3ac9a826dffdbc7232a3a2d230c9bbeaf7d9f5
Alissa Lawrence <alissa.lawrence@macys.com>
2015-10-27 11:45:07 -0700

    simplifying server for test

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ function handleResponse(req, res){
    var path = req.url;
    var response = "hello world";
    
-   switch(path){
-      case "/testingCallback":
-         response = "Thanks for calling me back!";
-	 console.log("response is: " + response);
-         break;
-      default:
-         break;
-   }
+   //switch(path){
+   //   case "/testingCallback":
+   //      response = "Thanks for calling me back!";
+  //	 console.log("response is: " + response);
+  //       break;
+  //    default:
+  //       break;
+  // }
 
    res.writeHead(200);
    res.end(response);
